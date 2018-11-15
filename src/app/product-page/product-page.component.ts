47fddd694dbf6fa5bba94fe520e268581b8ca274
arjun617 <arjundhall1995@gmail.com>
2018-11-15 15:02:18 +0530

    Update product-page.component.ts

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  selector: "app-product-page",
+  templateUrl: "./product-page.component.html",
+  styleUrls: ["./product-page.component.css"]
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  albumInfo;
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .subscribe(response => (this.albumInfo = response));
   }
-
 }
